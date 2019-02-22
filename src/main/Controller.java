7d7aa279fc4363d674cf84526220f224a14d0570
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2019-02-23 00:02:11 +0100

    Implemented controller Created two main scenes (newTable and showTable)

---
+++ src/main/Controller.java
@@ -0,0 +1,49 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private VBox mainWindow;
+
+    @FXML
+    private Button buttonShowTable;
+
+    @FXML
+    private Button buttonNewTable;
+
+    @FXML
+    private void createNewTable(ActionEvent e) throws IOException {
+        Stage stage = (Stage) buttonNewTable.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("newTable.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void showTable(ActionEvent e) throws IOException {
+        Stage stage = (Stage) buttonShowTable.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("showTable.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,13 +9,15 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage primaryStage) throws Exception {
 
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        primaryStage.setTitle("UniHours");
+        primaryStage.setScene(new Scene(root, 500, 475));
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
         launch(args);
